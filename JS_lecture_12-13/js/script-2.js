8c9884cc8cbb7ffcce1e63850ca0e36b067967cc
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2025-04-14 18:10:10 +0300

    add lecture 12-13

---
+++ JS_lecture_12-13/js/script-2.js
@@ -0,0 +1,11 @@
+// 	Навігація по DOM
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = document.querySelector(".list");
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);

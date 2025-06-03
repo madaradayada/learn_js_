0da439ede1232ee5a052822aef7f60c9beb7657d
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2025-06-03 09:30:18 +0300

    add example_5

---
+++ example/js/script-2.js
@@ -0,0 +1,22 @@
+// створюємо масив рядків з назвами інгридієнтів
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const list = document.querySelector("#ingredients");
+const liArray = [];
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.className = "item";
+  item.textContent = ingredient;
+  liArray.push(item);
+});
+
+list.append(...liArray);
